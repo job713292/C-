9feb6e2330130c90d8fcc9b39aead0b48d45705b
Job_Nuñez <jobnunez713@gmail.com>
2024-10-29 16:47:45 -0500

    Git C#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el capital inicial:");
+        decimal capital = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el número de meses:");
+        int meses = Convert.ToInt32(Console.ReadLine());
+
+        decimal tasaInteres = 0.007m;
+
+        for (int i = 1; i <= meses; i++)
+        {
+            capital += capital * tasaInteres;
+            Console.WriteLine($"Mes {i}: {capital:C2}");
+        }
+    }
+}
+
--- obj/Debug/net8.0/Trabajo-U.AssemblyInfo.cs
+++ obj/Debug/net8.0/Trabajo-U.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trabajo-U")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6d1587dd777a2b1f7f70c71ba25e4175165f77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trabajo-U")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trabajo-U")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
